1df5f549711db046c14f2584b25d7f40587007a0
vi-rossiyskaya <v.i.rossiyskaya@gmail.com>
2023-06-25 17:44:30 +0200

    feat: solve generateOdds

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -35,8 +35,8 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len }, (_, index) => 2 * index + 1);
 }
 
 
