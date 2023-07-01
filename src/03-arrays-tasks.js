76579977a433778a586fbd1d47ef125691cacd04
vi-rossiyskaya <v.i.rossiyskaya@gmail.com>
2023-07-02 00:10:48 +0200

    feat: add solution for sortDigitNamesByNumericOrder

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -329,8 +329,23 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const digitValues = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  arr.sort((a, b) => digitValues[a] - digitValues[b]);
+
+  return arr;
 }
 
 /**
